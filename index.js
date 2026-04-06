fcae4071424aacce3a4e952060b225b884bd200f
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-04-06 08:53:00 +0600

    Server Section Updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const connectDB = require('./src/config/db');
+const menuRoutes = require('./src/routes/menuRoutes');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,41 +10,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Connection (MongoDB Atlas theke link anben)
-// Database link-ta update korun (bou0ahg part-ta add hobe)
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bou0ahg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Main Function to Start Server
+async function startServer() {
+    const db = await connectDB(); 
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+    if (db) {
+        // Routes Setup
+        app.use('/menu', menuRoutes(db));
 
-async function run() {
-  try {
-    // Connect to MongoDB
-    await client.connect();
-    const menuCollection = client.db("Tasty_Twists_DB").collection("menu");
+        app.get('/', (req, res) => {
+            res.send('TastyTwists Server is Running Modularly..');
+        });
 
-    // 1. GET API (Database menu list)
-    app.get('/menu', async (req, res) => {
-        const result = await menuCollection.find().toArray();
-        res.send(result);
-    });
-
-    console.log("Pinged! Connected to MongoDB!");
-  } finally {
-   
-  }
+        app.listen(port, () => {
+            console.log(`Server running on port: ${port}`);
+        });
+    }
 }
-run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('TastyTwists Server is Running...');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+startServer();
