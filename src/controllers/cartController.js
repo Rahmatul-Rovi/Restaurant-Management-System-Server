c9352dccb9271bdd85346973ccf7df59e3168d02
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-04-11 00:28:28 +0600

    Server Section Updated

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -10,3 +10,16 @@ const addToCart = async (req,res,cartCollection) => {
    res.status(500).send({mesage: "Error adding to cart", error});
   }
 };
+
+
+const getCartByEmail = async (req, res, cartCollection) => {
+    try{
+      const email = req.query.email;
+      const query = {email: email};
+      const result = await cartCollection.findOne(query).toArray();
+      res.send(result);
+    }
+    catch(error){
+        res.status(500).send({error:"Error fetching Cart", error});
+    }
+};
