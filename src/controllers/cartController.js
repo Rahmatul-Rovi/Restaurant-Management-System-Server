9c7bd2f45b98f4d1793d13614891025559fbb31e
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-04-11 09:49:44 +0600

    Server Section Updated

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -23,3 +23,19 @@ const getCartByEmail = async (req, res, cartCollection) => {
         res.status(500).send({error:"Error fetching Cart", error});
     }
 };
+
+
+const deleteCartItem = async (req, res, cartCollection) => {
+  try{
+  const id = req.params.id;
+    const {ObjectId} = require('mongodb');
+    const query = {_id: newObjectId(id)};
+    const result = await cartCollection.deleteOne(query);
+    res.send(result);
+  }
+  catch(error){
+res.status(500).send({mesage: 'Error Deleting Item', error})
+    }
+};
+
+module.exports = {addToCart, getCartByEmail, deleteCartItem};
