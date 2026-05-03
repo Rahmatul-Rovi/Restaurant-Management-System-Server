4068ff1505304899f61422cbd0aeba3fc9b20c3a
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-05-03 09:21:34 +0600

    Server Section Updated

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { initiatePayment, paymentSuccess } = require('../controllers/paymentController');
 
-// পেমেন্ট শুরু করার জন্য POST রুট
+// এখন এই initiatePayment ফাংশনটি খুঁজে পাবে
 router.post('/order', initiatePayment);
 
-// পেমেন্ট সাকসেস হলে SSLCommerz এখানে ডাটা পাঠাবে
+// Stripe এর জন্য এটা আপাতত না রাখলেও চলে, তাও থাকলো এরর এড়াতে
 router.post('/payment/success/:tranId', paymentSuccess);
 
 module.exports = router;
